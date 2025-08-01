cfcc4b831c912e53b6bf918759e9ea5bf2b4e23e
rekinp <rekinp@wp.pl>
2025-08-01 14:54:49 +0200

    fix("Amplify has not been configured")

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import './globals.css';
+import { Amplify } from 'aws-amplify';
+import amplifyOutputs from '../../amplify_outputs.json'; // Gen2 outputs file
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import Providers from './Providers';
+
+Amplify.configure(amplifyOutputs);
 
 export const metadata = {
   title: 'Client Portal',
