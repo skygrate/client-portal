05eda63f434f4c10ec7f1f32efbefdc7960803f9
rekinp <rekinp@wp.pl>
2025-09-26 17:37:16 +0200

    fix(adjusted schema import path)

--- src/shared/client/amplify/client.ts
+++ src/shared/client/amplify/client.ts
@@ -1,7 +1,6 @@
 "use client";
 
 import { generateClient } from "aws-amplify/data";
-import type { Schema } from "../../../amplify/data/resource";
+import type { Schema } from "../../../../amplify/data/resource";
 
 export const amplifyClient = generateClient<Schema>();
-
